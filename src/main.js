e830dbd15166ff04fa87ae45e16604daa54b4177
lineouxue <3401075539@qq.com>
2024-12-27 16:11:24 +0800

    配置element

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// import './assets/main.css'
+
+import { createApp } from 'vue'
+// import pinia from '@/stores/index.js'
+
+import App from './App.vue'
+// import router from './router'
+
+const app = createApp(App)
+// app.use(router)
+// app.use(pinia)
+
+app.mount('#app')
+
+
+
